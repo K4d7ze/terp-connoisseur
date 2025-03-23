0b7700938b38524a1d79014e1d80c2d336f06a0b
K4d7ze <keyystr0ke@gmail.com>
2025-03-23 18:07:51 +0100

    Add files via upload

--- data.js
+++ data.js
@@ -46,5 +46,101 @@ window.strainData = [
         "effect": "fokussierend, bronchienerweiternd"
       }
     ]
+  },
+  {
+    "product": "Frosted Lemon Cake Auto",
+    "strain": "Frosted Lemon Cake",
+    "terpene": [
+      {
+        "name": "Limonene",
+        "boiling": 176,
+        "flavor": "zitronig, süß",
+        "effect": "stimmungsaufhellend, energetisierend"
+      },
+      {
+        "name": "Caryophyllene",
+        "boiling": 119,
+        "flavor": "würzig",
+        "effect": "entspannend, entzündungshemmend"
+      },
+      {
+        "name": "Myrcene",
+        "boiling": 166,
+        "flavor": "erdig",
+        "effect": "beruhigend, sedierend"
+      }
+    ]
+  },
+  {
+    "product": "Royal Queen Seeds - Watermelon Auto",
+    "strain": "Watermelon",
+    "terpene": [
+      {
+        "name": "Limonene",
+        "boiling": 176,
+        "flavor": "fruchtig, süß",
+        "effect": "euphorisierend, stresslösend"
+      },
+      {
+        "name": "Myrcene",
+        "boiling": 166,
+        "flavor": "erdig, moschusartig",
+        "effect": "körperlich entspannend"
+      },
+      {
+        "name": "Humulene",
+        "boiling": 198,
+        "flavor": "hopfig, holzig",
+        "effect": "entzündungshemmend, appetitzügelnd"
+      }
+    ]
+  },
+  {
+    "product": "Grape Galea",
+    "strain": "Grape Galea",
+    "terpene": [
+      {
+        "name": "Linalool",
+        "boiling": 198,
+        "flavor": "floral, lavendel",
+        "effect": "beruhigend, angstlösend"
+      },
+      {
+        "name": "Myrcene",
+        "boiling": 166,
+        "flavor": "erdig, traubig",
+        "effect": "sedierend"
+      },
+      {
+        "name": "Limonene",
+        "boiling": 176,
+        "flavor": "fruchtig",
+        "effect": "stimmungsaufhellend"
+      }
+    ]
+  },
+  {
+    "product": "Strawberry Diesel",
+    "strain": "Strawberry Diesel",
+    "terpene": [
+      {
+        "name": "Caryophyllene",
+        "boiling": 119,
+        "flavor": "würzig, diesel",
+        "effect": "entspannend"
+      },
+      {
+        "name": "Limonene",
+        "boiling": 176,
+        "flavor": "erdbeer-zitronig",
+        "effect": "anregend"
+      },
+      {
+        "name": "Myrcene",
+        "boiling": 166,
+        "flavor": "erdig",
+        "effect": "beruhigend"
+      }
+    ]
   }
 ];
