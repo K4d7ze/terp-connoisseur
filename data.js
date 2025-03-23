e7e73f32a4ff71bc61b5ed18bde2d9d8f2651714
K4d7ze <keyystr0ke@gmail.com>
2025-03-23 17:56:51 +0100

    Add files via upload

--- data.js
+++ data.js
@@ -1,34 +1,50 @@
-import json
-
-# Testdatenbank mit echten, validierten Einträgen zu 2 Fast Buds Strains
-
-test_strains = [
-    {
-        "product": "Fast Buds - Lemon Cherry Cookies Auto",
-        "strain": "Lemon Cherry Cookies",
-        "terpene": [
-            {"name": "Limonene", "boiling": 176, "flavor": "zitronig, süß", "effect": "euphorisierend, stimmungsaufhellend"},
-            {"name": "Caryophyllene", "boiling": 119, "flavor": "würzig, pfeffrig", "effect": "entspannend, entzündungshemmend"},
-            {"name": "Linalool", "boiling": 198, "flavor": "blumig", "effect": "beruhigend, angstlösend"}
-        ]
-    },
-    {
-        "product": "Fast Buds - Amnesia Haze Auto",
-        "strain": "Amnesia Haze",
-        "terpene": [
-            {"name": "Terpinolene", "boiling": 186, "flavor": "kräuterig, blumig", "effect": "energetisierend, kreativitätsfördernd"},
-            {"name": "Myrcene", "boiling": 166, "flavor": "erdig, moschusartig", "effect": "entspannend, sedierend"},
-            {"name": "Pinene", "boiling": 156, "flavor": "kieferartig", "effect": "fokussierend, bronchienerweiternd"}
-        ]
-    }
-]
-
-# Umwandeln in JS-kompatibles Format
-test_data_js = "window.strainData = " + json.dumps(test_strains, ensure_ascii=False, indent=2) + ";"
-
-# Speichern
-test_file_path = "/mnt/data/data_test_fastbuds.js"
-with open(test_file_path, "w", encoding="utf-8") as f:
-    f.write(test_data_js)
-
-test_file_path
+window.strainData = [
+  {
+    "product": "Fast Buds - Lemon Cherry Cookies Auto",
+    "strain": "Lemon Cherry Cookies",
+    "terpene": [
+      {
+        "name": "Limonene",
+        "boiling": 176,
+        "flavor": "zitronig, süß",
+        "effect": "euphorisierend, stimmungsaufhellend"
+      },
+      {
+        "name": "Caryophyllene",
+        "boiling": 119,
+        "flavor": "würzig, pfeffrig",
+        "effect": "entspannend, entzündungshemmend"
+      },
+      {
+        "name": "Linalool",
+        "boiling": 198,
+        "flavor": "blumig",
+        "effect": "beruhigend, angstlösend"
+      }
+    ]
+  },
+  {
+    "product": "Fast Buds - Amnesia Haze Auto",
+    "strain": "Amnesia Haze",
+    "terpene": [
+      {
+        "name": "Terpinolene",
+        "boiling": 186,
+        "flavor": "kräuterig, blumig",
+        "effect": "energetisierend, kreativitätsfördernd"
+      },
+      {
+        "name": "Myrcene",
+        "boiling": 166,
+        "flavor": "erdig, moschusartig",
+        "effect": "entspannend, sedierend"
+      },
+      {
+        "name": "Pinene",
+        "boiling": 156,
+        "flavor": "kieferartig",
+        "effect": "fokussierend, bronchienerweiternd"
+      }
+    ]
+  }
+];
