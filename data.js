b72acbc5c1c2cc16b61ba96dd3df24d1e988e9ba
K4d7ze <keyystr0ke@gmail.com>
2025-03-24 08:53:13 +0100

    Add files via upload

--- data.js
+++ data.js
@@ -1,144 +1,600 @@
 window.strainData = [
   {
-    "product": "Fast Buds - Lemon Cherry Cookies Auto",
-    "strain": "Lemon Cherry Cookies",
+    "product": "Tilray 22/1 Indica",
+    "strain": "Pink Kush",
     "terpene": [
-      {
-        "name": "Limonene",
-        "boiling": 176,
-        "flavor": "zitronig, süß",
-        "effect": "euphorisierend, stimmungsaufhellend"
-      },
-      {
-        "name": "Caryophyllene",
-        "boiling": 119,
-        "flavor": "würzig, pfeffrig",
-        "effect": "entspannend, entzündungshemmend"
-      },
-      {
-        "name": "Linalool",
-        "boiling": 198,
-        "flavor": "blumig",
-        "effect": "beruhigend, angstlösend"
-      }
-    ]
-  },
-  {
-    "product": "Fast Buds - Amnesia Haze Auto",
-    "strain": "Amnesia Haze",
-    "terpene": [
-      {
-        "name": "Terpinolene",
-        "boiling": 186,
-        "flavor": "kräuterig, blumig",
-        "effect": "energetisierend, kreativitätsfördernd"
-      },
-      {
-        "name": "Myrcene",
-        "boiling": 166,
-        "flavor": "erdig, moschusartig",
-        "effect": "entspannend, sedierend"
-      },
-      {
-        "name": "Pinene",
-        "boiling": 156,
-        "flavor": "kieferartig",
-        "effect": "fokussierend, bronchienerweiternd"
-      }
-    ]
-  },
-  {
-    "product": "Frosted Lemon Cake Auto",
-    "strain": "Frosted Lemon Cake",
-    "terpene": [
-      {
-        "name": "Limonene",
-        "boiling": 176,
-        "flavor": "zitronig, süß",
-        "effect": "stimmungsaufhellend, energetisierend"
-      },
       {
         "name": "Caryophyllene",
         "boiling": 119,
         "flavor": "würzig",
-        "effect": "entspannend, entzündungshemmend"
-      },
-      {
-        "name": "Myrcene",
-        "boiling": 166,
-        "flavor": "erdig",
-        "effect": "beruhigend, sedierend"
-      }
-    ]
-  },
-  {
-    "product": "Royal Queen Seeds - Watermelon Auto",
-    "strain": "Watermelon",
-    "terpene": [
-      {
-        "name": "Limonene",
-        "boiling": 176,
-        "flavor": "fruchtig, süß",
-        "effect": "euphorisierend, stresslösend"
-      },
-      {
-        "name": "Myrcene",
-        "boiling": 166,
-        "flavor": "erdig, moschusartig",
-        "effect": "körperlich entspannend"
-      },
-      {
-        "name": "Humulene",
-        "boiling": 198,
-        "flavor": "hopfig, holzig",
-        "effect": "entzündungshemmend, appetitzügelnd"
-      }
-    ]
-  },
-  {
-    "product": "Grape Galea",
-    "strain": "Grape Galea",
-    "terpene": [
-      {
-        "name": "Linalool",
-        "boiling": 198,
-        "flavor": "floral, lavendel",
-        "effect": "beruhigend, angstlösend"
-      },
-      {
-        "name": "Myrcene",
-        "boiling": 166,
-        "flavor": "erdig, traubig",
-        "effect": "sedierend"
-      },
-      {
-        "name": "Limonene",
-        "boiling": 176,
-        "flavor": "fruchtig",
-        "effect": "stimmungsaufhellend"
-      }
-    ]
-  },
-  {
-    "product": "Strawberry Diesel",
-    "strain": "Strawberry Diesel",
-    "terpene": [
-      {
-        "name": "Caryophyllene",
-        "boiling": 119,
-        "flavor": "würzig, diesel",
         "effect": "entspannend"
       },
       {
         "name": "Limonene",
         "boiling": 176,
-        "flavor": "erdbeer-zitronig",
-        "effect": "anregend"
+        "flavor": "zitronig",
+        "effect": "stimmungsaufhellend"
       },
       {
         "name": "Myrcene",
         "boiling": 166,
         "flavor": "erdig",
+        "effect": "beruhigend"
+      }
+    ]
+  },
+  {
+    "product": "Aurora 20/1 Hybrid",
+    "strain": "Banana OG",
+    "terpene": [
+      {
+        "name": "Limonene",
+        "boiling": 176,
+        "flavor": "bananig, süß",
+        "effect": "euphorisierend"
+      },
+      {
+        "name": "Myrcene",
+        "boiling": 166,
+        "flavor": "erdig",
+        "effect": "entspannend"
+      },
+      {
+        "name": "Linalool",
+        "boiling": 198,
+        "flavor": "blumig",
+        "effect": "angstlösend"
+      }
+    ]
+  },
+  {
+    "product": "Demecan 26/1 SAT CA",
+    "strain": "Jack Herer",
+    "terpene": [
+      {
+        "name": "Terpinolene",
+        "boiling": 186,
+        "flavor": "krautig",
+        "effect": "energetisierend"
+      },
+      {
+        "name": "Pinene",
+        "boiling": 156,
+        "flavor": "kieferartig",
+        "effect": "fokussierend"
+      },
+      {
+        "name": "Myrcene",
+        "boiling": 166,
+        "flavor": "erdig",
+        "effect": "entspannend"
+      }
+    ]
+  },
+  {
+    "product": "Enua 25/1 SBD CA",
+    "strain": "Strawberry Diesel",
+    "terpene": [
+      {
+        "name": "Limonene",
+        "boiling": 176,
+        "flavor": "zitronig, fruchtig",
+        "effect": "anregend"
+      },
+      {
+        "name": "Caryophyllene",
+        "boiling": 119,
+        "flavor": "dieselig",
+        "effect": "entspannend"
+      },
+      {
+        "name": "Myrcene",
+        "boiling": 166,
+        "flavor": "erdig",
+        "effect": "beruhigend"
+      }
+    ]
+  },
+  {
+    "product": "IMC 20/1 IND CA",
+    "strain": "Kosher Kush",
+    "terpene": [
+      {
+        "name": "Myrcene",
+        "boiling": 166,
+        "flavor": "moschusartig",
+        "effect": "sedierend"
+      },
+      {
+        "name": "Limonene",
+        "boiling": 176,
+        "flavor": "zitronig",
+        "effect": "stimmungsaufhellend"
+      },
+      {
+        "name": "Caryophyllene",
+        "boiling": 119,
+        "flavor": "würzig",
+        "effect": "beruhigend"
+      }
+    ]
+  },
+  {
+    "product": "Tilray 22/1 Indica",
+    "strain": "Pink Kush",
+    "terpene": [
+      {
+        "name": "Caryophyllene",
+        "boiling": 119,
+        "flavor": "würzig",
+        "effect": "entspannend"
+      },
+      {
+        "name": "Limonene",
+        "boiling": 176,
+        "flavor": "zitronig",
+        "effect": "stimmungsaufhellend"
+      },
+      {
+        "name": "Myrcene",
+        "boiling": 166,
+        "flavor": "erdig",
+        "effect": "beruhigend"
+      }
+    ]
+  },
+  {
+    "product": "Aurora 20/1 Hybrid",
+    "strain": "Banana OG",
+    "terpene": [
+      {
+        "name": "Limonene",
+        "boiling": 176,
+        "flavor": "bananig, süß",
+        "effect": "euphorisierend"
+      },
+      {
+        "name": "Myrcene",
+        "boiling": 166,
+        "flavor": "erdig",
+        "effect": "entspannend"
+      },
+      {
+        "name": "Linalool",
+        "boiling": 198,
+        "flavor": "blumig",
+        "effect": "angstlösend"
+      }
+    ]
+  },
+  {
+    "product": "Demecan 26/1 SAT CA",
+    "strain": "Jack Herer",
+    "terpene": [
+      {
+        "name": "Terpinolene",
+        "boiling": 186,
+        "flavor": "krautig",
+        "effect": "energetisierend"
+      },
+      {
+        "name": "Pinene",
+        "boiling": 156,
+        "flavor": "kieferartig",
+        "effect": "fokussierend"
+      },
+      {
+        "name": "Myrcene",
+        "boiling": 166,
+        "flavor": "erdig",
+        "effect": "entspannend"
+      }
+    ]
+  },
+  {
+    "product": "Enua 25/1 SBD CA",
+    "strain": "Strawberry Diesel",
+    "terpene": [
+      {
+        "name": "Limonene",
+        "boiling": 176,
+        "flavor": "zitronig, fruchtig",
+        "effect": "anregend"
+      },
+      {
+        "name": "Caryophyllene",
+        "boiling": 119,
+        "flavor": "dieselig",
+        "effect": "entspannend"
+      },
+      {
+        "name": "Myrcene",
+        "boiling": 166,
+        "flavor": "erdig",
+        "effect": "beruhigend"
+      }
+    ]
+  },
+  {
+    "product": "IMC 20/1 IND CA",
+    "strain": "Kosher Kush",
+    "terpene": [
+      {
+        "name": "Myrcene",
+        "boiling": 166,
+        "flavor": "moschusartig",
+        "effect": "sedierend"
+      },
+      {
+        "name": "Limonene",
+        "boiling": 176,
+        "flavor": "zitronig",
+        "effect": "stimmungsaufhellend"
+      },
+      {
+        "name": "Caryophyllene",
+        "boiling": 119,
+        "flavor": "würzig",
+        "effect": "beruhigend"
+      }
+    ]
+  },
+  {
+    "product": "Tilray 22/1 Indica",
+    "strain": "Pink Kush",
+    "terpene": [
+      {
+        "name": "Caryophyllene",
+        "boiling": 119,
+        "flavor": "würzig",
+        "effect": "entspannend"
+      },
+      {
+        "name": "Limonene",
+        "boiling": 176,
+        "flavor": "zitronig",
+        "effect": "stimmungsaufhellend"
+      },
+      {
+        "name": "Myrcene",
+        "boiling": 166,
+        "flavor": "erdig",
+        "effect": "beruhigend"
+      }
+    ]
+  },
+  {
+    "product": "Aurora 20/1 Hybrid",
+    "strain": "Banana OG",
+    "terpene": [
+      {
+        "name": "Limonene",
+        "boiling": 176,
+        "flavor": "bananig, süß",
+        "effect": "euphorisierend"
+      },
+      {
+        "name": "Myrcene",
+        "boiling": 166,
+        "flavor": "erdig",
+        "effect": "entspannend"
+      },
+      {
+        "name": "Linalool",
+        "boiling": 198,
+        "flavor": "blumig",
+        "effect": "angstlösend"
+      }
+    ]
+  },
+  {
+    "product": "Demecan 26/1 SAT CA",
+    "strain": "Jack Herer",
+    "terpene": [
+      {
+        "name": "Terpinolene",
+        "boiling": 186,
+        "flavor": "krautig",
+        "effect": "energetisierend"
+      },
+      {
+        "name": "Pinene",
+        "boiling": 156,
+        "flavor": "kieferartig",
+        "effect": "fokussierend"
+      },
+      {
+        "name": "Myrcene",
+        "boiling": 166,
+        "flavor": "erdig",
+        "effect": "entspannend"
+      }
+    ]
+  },
+  {
+    "product": "Enua 25/1 SBD CA",
+    "strain": "Strawberry Diesel",
+    "terpene": [
+      {
+        "name": "Limonene",
+        "boiling": 176,
+        "flavor": "zitronig, fruchtig",
+        "effect": "anregend"
+      },
+      {
+        "name": "Caryophyllene",
+        "boiling": 119,
+        "flavor": "dieselig",
+        "effect": "entspannend"
+      },
+      {
+        "name": "Myrcene",
+        "boiling": 166,
+        "flavor": "erdig",
+        "effect": "beruhigend"
+      }
+    ]
+  },
+  {
+    "product": "IMC 20/1 IND CA",
+    "strain": "Kosher Kush",
+    "terpene": [
+      {
+        "name": "Myrcene",
+        "boiling": 166,
+        "flavor": "moschusartig",
+        "effect": "sedierend"
+      },
+      {
+        "name": "Limonene",
+        "boiling": 176,
+        "flavor": "zitronig",
+        "effect": "stimmungsaufhellend"
+      },
+      {
+        "name": "Caryophyllene",
+        "boiling": 119,
+        "flavor": "würzig",
+        "effect": "beruhigend"
+      }
+    ]
+  },
+  {
+    "product": "Tilray 22/1 Indica",
+    "strain": "Pink Kush",
+    "terpene": [
+      {
+        "name": "Caryophyllene",
+        "boiling": 119,
+        "flavor": "würzig",
+        "effect": "entspannend"
+      },
+      {
+        "name": "Limonene",
+        "boiling": 176,
+        "flavor": "zitronig",
+        "effect": "stimmungsaufhellend"
+      },
+      {
+        "name": "Myrcene",
+        "boiling": 166,
+        "flavor": "erdig",
+        "effect": "beruhigend"
+      }
+    ]
+  },
+  {
+    "product": "Aurora 20/1 Hybrid",
+    "strain": "Banana OG",
+    "terpene": [
+      {
+        "name": "Limonene",
+        "boiling": 176,
+        "flavor": "bananig, süß",
+        "effect": "euphorisierend"
+      },
+      {
+        "name": "Myrcene",
+        "boiling": 166,
+        "flavor": "erdig",
+        "effect": "entspannend"
+      },
+      {
+        "name": "Linalool",
+        "boiling": 198,
+        "flavor": "blumig",
+        "effect": "angstlösend"
+      }
+    ]
+  },
+  {
+    "product": "Demecan 26/1 SAT CA",
+    "strain": "Jack Herer",
+    "terpene": [
+      {
+        "name": "Terpinolene",
+        "boiling": 186,
+        "flavor": "krautig",
+        "effect": "energetisierend"
+      },
+      {
+        "name": "Pinene",
+        "boiling": 156,
+        "flavor": "kieferartig",
+        "effect": "fokussierend"
+      },
+      {
+        "name": "Myrcene",
+        "boiling": 166,
+        "flavor": "erdig",
+        "effect": "entspannend"
+      }
+    ]
+  },
+  {
+    "product": "Enua 25/1 SBD CA",
+    "strain": "Strawberry Diesel",
+    "terpene": [
+      {
+        "name": "Limonene",
+        "boiling": 176,
+        "flavor": "zitronig, fruchtig",
+        "effect": "anregend"
+      },
+      {
+        "name": "Caryophyllene",
+        "boiling": 119,
+        "flavor": "dieselig",
+        "effect": "entspannend"
+      },
+      {
+        "name": "Myrcene",
+        "boiling": 166,
+        "flavor": "erdig",
+        "effect": "beruhigend"
+      }
+    ]
+  },
+  {
+    "product": "IMC 20/1 IND CA",
+    "strain": "Kosher Kush",
+    "terpene": [
+      {
+        "name": "Myrcene",
+        "boiling": 166,
+        "flavor": "moschusartig",
+        "effect": "sedierend"
+      },
+      {
+        "name": "Limonene",
+        "boiling": 176,
+        "flavor": "zitronig",
+        "effect": "stimmungsaufhellend"
+      },
+      {
+        "name": "Caryophyllene",
+        "boiling": 119,
+        "flavor": "würzig",
+        "effect": "beruhigend"
+      }
+    ]
+  },
+  {
+    "product": "Tilray 22/1 Indica",
+    "strain": "Pink Kush",
+    "terpene": [
+      {
+        "name": "Caryophyllene",
+        "boiling": 119,
+        "flavor": "würzig",
+        "effect": "entspannend"
+      },
+      {
+        "name": "Limonene",
+        "boiling": 176,
+        "flavor": "zitronig",
+        "effect": "stimmungsaufhellend"
+      },
+      {
+        "name": "Myrcene",
+        "boiling": 166,
+        "flavor": "erdig",
+        "effect": "beruhigend"
+      }
+    ]
+  },
+  {
+    "product": "Aurora 20/1 Hybrid",
+    "strain": "Banana OG",
+    "terpene": [
+      {
+        "name": "Limonene",
+        "boiling": 176,
+        "flavor": "bananig, süß",
+        "effect": "euphorisierend"
+      },
+      {
+        "name": "Myrcene",
+        "boiling": 166,
+        "flavor": "erdig",
+        "effect": "entspannend"
+      },
+      {
+        "name": "Linalool",
+        "boiling": 198,
+        "flavor": "blumig",
+        "effect": "angstlösend"
+      }
+    ]
+  },
+  {
+    "product": "Demecan 26/1 SAT CA",
+    "strain": "Jack Herer",
+    "terpene": [
+      {
+        "name": "Terpinolene",
+        "boiling": 186,
+        "flavor": "krautig",
+        "effect": "energetisierend"
+      },
+      {
+        "name": "Pinene",
+        "boiling": 156,
+        "flavor": "kieferartig",
+        "effect": "fokussierend"
+      },
+      {
+        "name": "Myrcene",
+        "boiling": 166,
+        "flavor": "erdig",
+        "effect": "entspannend"
+      }
+    ]
+  },
+  {
+    "product": "Enua 25/1 SBD CA",
+    "strain": "Strawberry Diesel",
+    "terpene": [
+      {
+        "name": "Limonene",
+        "boiling": 176,
+        "flavor": "zitronig, fruchtig",
+        "effect": "anregend"
+      },
+      {
+        "name": "Caryophyllene",
+        "boiling": 119,
+        "flavor": "dieselig",
+        "effect": "entspannend"
+      },
+      {
+        "name": "Myrcene",
+        "boiling": 166,
+        "flavor": "erdig",
+        "effect": "beruhigend"
+      }
+    ]
+  },
+  {
+    "product": "IMC 20/1 IND CA",
+    "strain": "Kosher Kush",
+    "terpene": [
+      {
+        "name": "Myrcene",
+        "boiling": 166,
+        "flavor": "moschusartig",
+        "effect": "sedierend"
+      },
+      {
+        "name": "Limonene",
+        "boiling": 176,
+        "flavor": "zitronig",
+        "effect": "stimmungsaufhellend"
+      },
+      {
+        "name": "Caryophyllene",
+        "boiling": 119,
+        "flavor": "würzig",
         "effect": "beruhigend"
       }
     ]
